2558b79da6f98bd04728eac73e73875c194c09d5
timmyreilly <timreilly@live.com>
2015-07-01 15:57:10 -0700

    added cloud images

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #main.py
 
+from gevent import monkey
+monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, disconnect
